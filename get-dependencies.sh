0207fff393fccf1b62083fe7d3d4f101d2149107
Link4Electronics <link4electronics@gmail.com>
2026-03-20 23:27:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    libdecor       \
     pipewire-audio \
     pipewire-jack  \
     sdl2
