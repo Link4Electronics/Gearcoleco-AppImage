952bba35aec4e0566e9c72eeba0a4001c5e7c109
Link4Electronics <link4electronics@gmail.com>
2026-04-29 01:30:53 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,24 +7,32 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor       \
-    pipewire-alsa  \
-    pipewire-audio \
-    sdl2
+    libdecor \
+    sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package gearcoleco
+#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+case "$ARCH" in # they use x64 and arm64 for the zip links
+	x86_64)  farch=x64;;
+	aarch64) farch=arm64;;
+esac
+ZIP_LINK=$(wget https://api.github.com/repos/drhelius/Gearcoleco/releases -O - \
+      | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*ubuntu24.04-$farch.zip")
+echo "$ZIP_LINK" | awk -F'/' '{gsub(/^v/, "", $(NF-1)); print $(NF-1); exit}' > ~/version
+if ! wget --retry-connrefused --tries=30 "$ZIP_LINK" -O /tmp/app.zip 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+bsdtar -xvf /tmp/app.zip -C ./AppDir/bin
+rm -f ./AppDir/bin/README.txt
+wget -O ./AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
